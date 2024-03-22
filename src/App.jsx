fb42374c228bc27f28f68e99ef527c3c25022d77
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-22 11:56:26 +0600

    Created a list of components to create connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import "./App.css";
-import ReusableForm from "./components/ReusableForm/ReusableForm";
+import GrandFather from "./components/GrandFather/GrandFather";
+// import ReusableForm from "./components/ReusableForm/ReusableForm";
 // import HookForm from "./components/HookForm/HookForm";
 // import RefForm from "./components/RefForm/RefForm";
 // import StatefulForm from "./components/StatefulForm/StatefulForm";
 // import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
-  const handleSignUpSubmit = (data) => {
-    console.log("Sign Up Data", data)
-  };
+  // const handleSignUpSubmit = (data) => {
+  //   console.log("Sign Up Data", data)
+  // };
 
-  const handleProfileUpdateSubmit = (data) => {
-    console.log("Update Profile", data)
-  };
+  // const handleProfileUpdateSubmit = (data) => {
+  //   console.log("Update Profile", data)
+  // };
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Form Master</h1>
+      <GrandFather></GrandFather>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
       {/* <HookForm></HookForm> */}
-      <ReusableForm
+      {/* <ReusableForm
         formTitle={"Sign Up"}
         handleSubmit={handleSignUpSubmit}
       >
@@ -39,7 +41,7 @@ function App() {
           <h2>Update Profile</h2>
           <p>Always keep your profile updated</p>
         </div>
-      </ReusableForm>
+      </ReusableForm> */}
     </>
   );
 }
